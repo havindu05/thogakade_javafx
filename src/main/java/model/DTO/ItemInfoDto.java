5c1e23054943c32e8e9112f4e3b9edf4b0506700
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-19 19:29:19 +0530

    Create itemInfoDto

---
+++ src/main/java/model/DTO/ItemInfoDto.java
@@ -0,0 +1,9 @@
+package model.DTO;
+
+public class ItemInfoDto {
+    private String itemCode;
+    private String description;
+    private String category;
+    private int qtyOnHand;
+    private double unitPrice;
+}
