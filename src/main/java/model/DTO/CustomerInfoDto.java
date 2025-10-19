76752e263b03119ca005f0af6ad0facea58e48fe
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-19 19:24:56 +0530

    Create CustomerInfoDto

---
+++ src/main/java/model/DTO/CustomerInfoDto.java
@@ -0,0 +1,20 @@
+package model.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustomerInfoDto {
+    private String CustId;
+    private String title;
+    private String name;
+    private String dob;
+    private double slaray;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
+}
