a0698e60286be711f383a07e49a6a6efb227d48b
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-19 18:43:49 +0530

    Create HomePage

--- src/main/java/Controller/HomePageController.java
+++ src/main/java/Controller/HomePageController.java
@@ -1,4 +1,28 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class HomePageController {
+
+    @FXML
+    void btnCustomerManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeemanagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierManagemnt(ActionEvent event) {
+
+    }
+
 }
