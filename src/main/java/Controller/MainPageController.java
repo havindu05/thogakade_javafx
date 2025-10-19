3981854c9eb7dc12969fce1ac5df8acd9b41a99c
Havindu Binduhewa <116324272+havindu05@users.noreply.github.com>
2025-10-19 16:40:51 +0530

    Start to create login and signup pages

--- src/main/java/Controller/MainPageController.java
+++ src/main/java/Controller/MainPageController.java
@@ -1,4 +1,36 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainPageController {
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/CustomerInfoDes.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("CustomerInfoDescribed");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void btnSignUp(ActionEvent event) {
+
+    }
+
 }
